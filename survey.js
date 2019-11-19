60115c10227cdc7c6475e44e9728b6af47d0f914
keramcgovern <keramcgovern@gmail.com>
2019-11-19 12:59:38 -0500

    added comments

--- survey.js
+++ survey.js
@@ -1,5 +1,11 @@
-
+/* List of the values that the user submitted on the form*/
 var user  = []
+
+/* Each Dorm Halls answers to the questions 
+
+Order: bathroom, ac, cost, class closeness, lounge, view, storage, co-ed, RA
+
+NOTE: need real values here */
 var Davis  = [1,0,1,1,0,1,0,1,1]
 var CPH    = [0,1,1,1,0,0,1,1,1]
 var Palmer = [0,1,1,1,0,0,1,1,1]
@@ -9,6 +15,7 @@ var Humps  = [0,1,1,1,0,0,1,1,1]
 
 var dormValues = [Davis, CPH, Palmer, River, Jonas, Humps];
 
+/* Scores for each dorm */
 var dScore = 0;
 var cScore = 0;
 var pScore = 0;
@@ -16,8 +23,11 @@ var rScore = 0;
 var jScore = 0;
 var hScore = 0;
 
+/* List of the final scores of the dorm halls
+Order: Davis, CPH, Palmer, River, Jonas, Humps */
 var dormScores = [0, 0, 0, 0, 0, 0];
 
+/* Uses the Dorm hall answers to calculate the score for each hall */
 function getScore(input){
     for (i=0; i<dormScores.length; i++){
         var total = 0;
@@ -26,8 +36,32 @@ function getScore(input){
         }
         dormScores[i] = total;
     }
+    setScores(dormScores);
 }
 
+/* Sets the score equal to the score calcuated in getScore() */
+function setScores(input){
+    dScore = input[0];
+    cScore = input[1];
+    pScore = input[2];
+    rScore = input[3];
+    jScore = input[4];
+    hScore = input[5];
+}
+
+/* Will order the list of dorms according to the lowest score
+
+NOTE:
+Within duckDormsForm.html, the value assigned for each question is opposite of what
+was expected in the first place. (Originally it was thought that the highest score
+would be the closest match but it reality, it is the lowest score that is the closest match) */
+function findMatches(){
+    
+}
+
+/* When the form is submitted, this function runs. This function check to make sure that
+a name, grade, and all the questions are answered. If all elements are filled out, it will
+run the rest of the functions to determine the order of results for them. */
 function validateForm(){
     var name = document.getElementById("name").value
     var grade = document.getElementById("grade").value
@@ -47,6 +81,7 @@ function validateForm(){
         document.getElementById("error").innerHTML = "Please Fill Out the Form Completely.";
     }
     else{
-        final = getScore(user).sort().reverse();
+        getScore(user);
+        findMatches();
     }
 }
